2736092ac859f6b8deba2a3f6f262d25fc0f15f8
igorkang-smc <ikanv13@gmail.com>
2025-06-22 02:16:28 +0900

    rsschool-devops-task1

---
+++ task_1/iam-github.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "gha_trust" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:igorkang-smc/rsschool-devops-course-tasks:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "GithubActionsRole"
+  assume_role_policy = data.aws_iam_policy_document.gha_trust.json
+}
+
+locals {
+  pols = [
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  for_each   = toset(local.pols)
+  role       = aws_iam_role.github_actions.name
+  policy_arn = each.value
+}
---
+++ task_1/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "rsschool-tf-state-12167"
+}
