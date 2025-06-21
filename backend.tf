b0e9fc953a1c80c50cc95a6c555d926b6c99eb92
igorkang-smc <ikanv13@gmail.com>
2025-06-22 02:38:26 +0900

    rsschool-devops-task1

--- backend.tf
+++ backend.tf
@@ -3,9 +3,9 @@ terraform {
   required_version = ">= 1.6"
 
   backend "s3" {
-    bucket         = "rsschool-tf-state-12167"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
+    bucket  = "rsschool-tf-state-12167"
+    key     = "global/s3/terraform.tfstate"
+    region  = "ap-northeast-2"
+    encrypt = true
   }
 }
